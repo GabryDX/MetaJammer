44219bd3593f1603c9380eb098299ceb60fcdef4
GabryDX <gabrydx@hotmail.com>
2026-06-16 20:36:56 +0200

    Update version to 0.3.0

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -13,7 +13,7 @@ android {
         minSdk = 33
         targetSdk = 37
         versionCode = 1
-        versionName = "0.2.4"
+        versionName = "0.3.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
